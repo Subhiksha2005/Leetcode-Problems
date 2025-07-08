d50b40c49cc51b8d7f337aab71be5745fa487c96
Subhiksha2005 <subhi42005@gmail.com>
2025-07-08 17:45:08 +0530

    Create 202. Happy Number.java

---
+++ 202. Happy Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            int s = 0;
+            while (n != 0) {
+                int a = n % 10;
+                s += a * a;
+                n = n / 10;
+            }
+            n = s;
+        }
+        return n == 1;
+    }
+}
